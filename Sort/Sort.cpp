e1a9c0130431f3828c085a22c4a002f8892a9f43
JamGrif <40271350+JamGrif@users.noreply.github.com>
2019-09-13 23:28:02 +0100

    Clean

--- Sort/Sort.cpp
+++ Sort/Sort.cpp
@@ -1,5 +1,43 @@
-void BubbleSort();
-void PrintSort();
+#include <iostream>
+using namespace std;
+
+int const ArraySize = 30;
+int Sort[ArraySize] = {8234,33564,1422,29,1440,35644,72325,22,934,113,342,654,5423,547,4235,5647,354,1342,1,543,67,3543564,87,0,23,987654,23,5647,3564,123};
+
+void PrintSort()
+{
+	for (int i = 0; i < ArraySize; i++)
+	{
+		cout << "Position " << i << " in the array is " << Sort[i] << "!" << endl;
+	}
+}
+
+void BubbleSort()
+{
+	int x = 0;
+	int y = 1;
+	int firstnum = 0;
+	int secondnum = 0;
+	for (int j = 0; j < ArraySize * ArraySize; j++)
+	{
+		x = 0;
+		y = 1;
+		for (int i = 1; i < ArraySize; i++)
+		{
+			if (Sort[x] > Sort[y])
+			{
+				firstnum = Sort[x];
+				secondnum = Sort[y];
+				Sort[x] = secondnum;
+				Sort[y] = firstnum;
+			}
+			x++;
+			y++;
+		}
+	}
+}
+
+
 
 int main()
 {
